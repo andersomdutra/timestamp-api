c481dee739be2dd444c0759825172aadd1c3db70
Andersom Rodrigues <andersom.rfd@hotmail.com>
2017-04-03 08:35:41 +0000

    timestamp microservice

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/*', function (req, res) {
+  var param = req.params[0]
+  
+  if (!isNaN(param)) {
+    param = parseInt(param)
+  }
+  
+  var date = new Date(param)
+  
+  if (!isFinite(date)) {
+    res.json({unix: null, natural: null})
+  }
+  
+  res.json({unix: date.getTime(), natural: date.toDateString()})
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
